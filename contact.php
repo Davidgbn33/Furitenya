d4ae37ef24a8df0a6c069bc531569a6e4eb9ffaf
Gibon_david <davidgbn33@gmail.com>
2023-08-04 11:25:11 +0200

    first commit

---
+++ contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<!-- head -->
+<?php include 'includes/head.php'; ?>
+<body>
+<!-- Navigation-->
+<?php include 'includes/navbar.php'; ?>
+
+<!-- Page Header-->
+<?php include 'includes/header.php'; ?>
+
+<!-- Main Content-->
+<?php include 'includes/contact.php'; ?>
+
+<!-- Footer-->
+<?php include 'includes/footer.php'; ?>
+
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+<footer class="border-top">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item">
+                        <a href="https://discord.com/channels/560855529661136913/1019002701335498812">
+                                    <span class="fa-stack fa-lg">
+                                        <i class="fas fa-circle fa-stack-2x"></i>
+                                        <i class="fab fa-discord fa-stack-1x fa-inverse"></i>
+                                    </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href=" https://www.facebook.com/Furitenya/">
+                                    <span class="fa-stack fa-lg">
+                                        <i class="fas fa-circle fa-stack-2x"></i>
+                                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                                    </span>
+                        </a>
+                    </li>
+                </ul>
+                <div class="small text-center text-muted fst-italic">Copyright &copy; Gibon David 2023</div>
+            </div>
+        </div>
+    </div>
+</footer>
